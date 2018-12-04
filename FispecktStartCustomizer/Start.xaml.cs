d78b4aec724c346a979ca051316fe60c3a1ab179
zziger <zziger07@gmail.com>
2018-12-04 21:50:52 +0100

    some actions

--- FispecktStartCustomizer/Start.xaml.cs
+++ FispecktStartCustomizer/Start.xaml.cs
@@ -1,30 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace FispecktStartCustomizer
 {
     /// <summary>
-    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// Логика взаимодействия для Start.xaml
     /// </summary>
-    public sealed partial class Start : Page
+    public partial class Start
     {
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("SHELL32", CallingConvention = CallingConvention.StdCall)]
+        public static extern int SHAppBarMessage(int dwMessage, ref APPBARDATA pData);
+
+
         public Start()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            //this.Height = System.Windows.SystemParameters.WorkArea.Height-150;
+            MessageBox.Show(GetTaskBarPos().ToString());
+        }
+
+        private int GetTaskBarPos()
+        {
+            IntPtr hwnd = FindWindow("Shell_TrayWnd", null);
+            if (hwnd != null)
+            {
+                APPBARDATA abd = new APPBARDATA();
+                abd.cbSize = Marshal.SizeOf(abd);
+                abd.hWnd = hwnd;
+                SHAppBarMessage((int)ABMsg.ABM_GETTASKBARPOS, ref abd);
+                SHAppBarMessage((int)ABMsg.ABM_QUERYPOS, ref abd);
+                int uEdge = GetEdge(abd.rc);
+                MessageBox.Show(abd.uEdge.ToString());
+                return uEdge;
+            }
+            else return 3;
+        }
+
+        private int GetEdge(RECT rc)
+        {
+            int uEdge = -1;
+            if (rc.top == rc.left && rc.bottom > rc.right)
+                uEdge = (int)ABEdge.ABE_LEFT;
+            else if (rc.top == rc.left && rc.bottom < rc.right)
+                uEdge = (int)ABEdge.ABE_TOP;
+            else if (rc.top > rc.left)
+                uEdge = (int)ABEdge.ABE_BOTTOM;
+            else
+                uEdge = (int)ABEdge.ABE_RIGHT;
+            return uEdge;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct APPBARDATA
+        {
+            public int cbSize;
+            public IntPtr hWnd;
+            public int uCallbackMessage;
+            public int uEdge;
+            public RECT rc;
+            public bool lParam;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        public enum ABEdge
+        {
+            ABE_LEFT = 0,
+            ABE_TOP = 1,
+            ABE_RIGHT = 2,
+            ABE_BOTTOM = 3
+        }
+
+        public enum ABMsg
+        {
+            ABM_NEW = 0,
+            ABM_REMOVE = 1,
+            ABM_QUERYPOS = 2,
+            ABM_SETPOS = 3,
+            ABM_GETSTATE = 4,
+            ABM_GETTASKBARPOS = 5,
+            ABM_ACTIVATE = 6,
+            ABM_GETAUTOHIDEBAR = 7,
+            ABM_SETAUTOHIDEBAR = 8,
+            ABM_WINDOWPOSCHANGED = 9,
+            ABM_SETSTATE = 10
         }
     }
 }
