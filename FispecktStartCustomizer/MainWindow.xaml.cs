c78e001038c8be364fac550c35bc35f60479901b
localChicken <localcmail@gmail.com>
2018-12-10 17:46:31 +0300

    Added keyboard detection, code cleanup

--- FispecktStartCustomizer/MainWindow.xaml.cs
+++ FispecktStartCustomizer/MainWindow.xaml.cs
@@ -1,21 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Automation;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
-using System.Threading;
 using System.ComponentModel;
 
 namespace FispecktStartCustomizer
@@ -28,49 +14,17 @@ namespace FispecktStartCustomizer
 
     public partial class MainWindow
     {
-        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
-        private static LowLevelMouseProc proc = hCallback;
-        private static IntPtr hookId = IntPtr.Zero;
+
+        /***********************************************************************Mouse hook for start button***********************************************************************/
+
+        //Variables for mousehook
+
+        private static IntPtr mHookId = IntPtr.Zero;
+
+        private static HookProc mouseProc = mouseCallback;
+
         private static Win32Point curPos = new Win32Point();
         private static Rect buttonRect = new Rect();
-
-        /***********************************************************************Mouse hook for start button/***********************************************************************/
-        public void Hook()
-        {
-            hookId = HookMouse(proc);
-        }
-
-        private void UnHook()
-        {
-            UnhookWindowsHookEx(hookId);
-        }
-
-        private static IntPtr HookMouse(LowLevelMouseProc proc)
-        {
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
-            {
-                return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
-            }
-        }
-
-        private static IntPtr hCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode >= 0 && MouseMessages.WM_LBUTTONUP == (MouseMessages)wParam)
-            {
-                GetCursorPos(ref curPos);
-                Console.WriteLine(curPos.X + " " + buttonRect.Left + " " + buttonRect.Right); // Debug information
-                if (curPos.X >= buttonRect.Left && curPos.X <= buttonRect.Right)
-                {
-                    if (curPos.Y >= buttonRect.Top && curPos.Y <= buttonRect.Bottom)
-                    {
-                        Console.WriteLine("Clicked"); // Mouse clicked on start menu button
-                    }
-                }
-
-            }
-            return CallNextHookEx(hookId, nCode, wParam, lParam);
-        }
 
         private const int WH_MOUSE_LL = 14;
 
@@ -84,28 +38,129 @@ namespace FispecktStartCustomizer
             public Int32 X;
             public Int32 Y;
         };
+        private enum MouseMessages { WM_LBUTTONUP = 0x0202 }
+
+        private static IntPtr HookMouse(HookProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr mouseCallback(int nCode, IntPtr wParam, KBDLLHOOKSTRUCT lParam)
+        {
+            if (nCode >= 0 && (MouseMessages)wParam == MouseMessages.WM_LBUTTONUP)
+            {
+                GetCursorPos(ref curPos);
+                Console.WriteLine(curPos.X + " " + buttonRect.Left + " " + buttonRect.Right); // Debug information
+                if (curPos.X >= buttonRect.Left && curPos.X <= buttonRect.Right)
+                {
+                    if (curPos.Y >= buttonRect.Top && curPos.Y <= buttonRect.Bottom)
+                    {
+                        Console.WriteLine("Clicked"); // Mouse clicked on start menu button
+                    }
+                }
+
+            }
+            return CallNextHookEx(mHookId, nCode, wParam, lParam);
+        }
+
         /***********************************************************************End of mouse hook***********************************************************************/
 
 
+        /***********************************************************************Keyboard hook***********************************************************************/
+
+        private static IntPtr keyHookId = IntPtr.Zero;
+
+        private const int WH_KEYBOARD_LL = 13; // Low-level keyboard hook number
+        private static HookProc keyboardProc = keyboardCallback;
+
+        private enum KeyMessages { WM_KEYUP = 0x0101, VK_LWIN=0x5B, VK_RWIN=0x5C }
+
+        private static IntPtr HookKeyboard(HookProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+
+        private static IntPtr keyboardCallback(int nCode, IntPtr wParam, KBDLLHOOKSTRUCT lParam)
+        {
+            if(nCode >= 0 && (KeyMessages)wParam == KeyMessages.WM_KEYUP)
+            {
+                if((KeyMessages)lParam.vkCode == KeyMessages.VK_LWIN | (KeyMessages)lParam.vkCode == KeyMessages.VK_RWIN)
+                {
+                    Console.WriteLine("Start pressed");
+
+                }
+            }
+            return (IntPtr)1;
+        }
+
+        /***********************************************************************End of keyboard hook***********************************************************************/
+
+
         /***********************************************************************Implementing functions for hoooks***********************************************************************/
-        private enum MouseMessages { WM_LBUTTONUP = 0x0202 }
+
+
+        delegate IntPtr HookProc(int code, IntPtr wParam, KBDLLHOOKSTRUCT lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class KBDLLHOOKSTRUCT
+        {
+            public uint vkCode;
+            public uint scanCode;
+            public KBDLLHOOKSTRUCTFlags flags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        [Flags]
+        public enum KBDLLHOOKSTRUCTFlags : uint
+        {
+            LLKHF_EXTENDED = 0x01,
+            LLKHF_INJECTED = 0x10,
+            LLKHF_ALTDOWN = 0x20,
+            LLKHF_UP = 0x80,
+        }
 
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int hookId, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+        private static extern IntPtr SetWindowsHookEx(int hookId, HookProc lpfn, IntPtr hMod, uint dwThreadId);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, KBDLLHOOKSTRUCT lParam);
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
 
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
         /***********************************************************************End of implementing functions***********************************************************************/
 
+        /***********************************************************************Hook control functions***********************************************************************/
+        public void Hook()
+        {
+            mHookId = HookMouse(mouseProc);
+            keyHookId = HookKeyboard(keyboardProc);
+        }
+
+        private void UnHook()
+        {
+            UnhookWindowsHookEx(mHookId);
+            UnhookWindowsHookEx(keyHookId);
+        }
 
         //Import some functions for start menu tooltip 
         [DllImport("user32.dll")]
@@ -127,7 +182,7 @@ namespace FispecktStartCustomizer
         public MainWindow()
         {
         }
-        
+       
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -137,6 +192,7 @@ namespace FispecktStartCustomizer
                 IntPtr hwndButton = FindWindowEx(TaskBar, IntPtr.Zero, "Start", null); // Connect to start menu tooltip
                 if (hwndButton != (IntPtr)0)
                 {
+                    EnableWindow(hwndButton, false);
                     GetWindowRect(hwndButton, ref buttonRect);
                     Hook();
                 }
