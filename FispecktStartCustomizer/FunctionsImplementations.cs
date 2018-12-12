7177397ac53ed58a4010e964ee0912d4155d4b70
localChicken <localcmail@gmail.com>
2018-12-12 17:12:00 +0300

    Config fix

---
+++ FispecktStartCustomizer/FunctionsImplementations.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace FispecktStartCustomizer
+{
+    public class FunctionsImplementations
+    {
+        public delegate IntPtr HookProc(int code, IntPtr wParam, KBDLLHOOKSTRUCT lParam);
+
+        public struct Size
+        {
+            public int width;
+            public int height;
+        } 
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class KBDLLHOOKSTRUCT
+        {
+            public uint vkCode;
+            public uint scanCode;
+            public KBDLLHOOKSTRUCTFlags flags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        [Flags]
+        public enum KBDLLHOOKSTRUCTFlags : uint
+        {
+            LLKHF_EXTENDED = 0x01,
+            LLKHF_INJECTED = 0x10,
+            LLKHF_ALTDOWN = 0x20,
+            LLKHF_UP = 0x80,
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int hookId, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
+        //Functions for start menu tooltip
+
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string ClassName, string WindowName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string className, string windowName);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+    }
+}
---
+++ FispecktStartCustomizer/StartMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FispecktStartCustomizer
+{
+    /// <summary>
+    /// Логика взаимодействия для StartMenu.xaml
+    /// </summary>
+    public partial class StartMenu
+    {
+        public void AddButton(String imageSrc, String path, FunctionsImplementations.Size size, int x, int y)
+        {
+            Button b = new Button();
+            b.Style = Application.Current.Resources["ButtonRevealStyle"] as Style;
+            b.Content = new BitmapImage(new Uri(imageSrc));
+            b.AddHandler(Button.ClickEvent, new RoutedEventHandler(b_Click));
+            grid.Children.Add(b);
+
+        }
+
+        private void b_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        public StartMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
